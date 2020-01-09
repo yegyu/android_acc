d3c4984c6708c08f32aa30658fb3a01eeb6bb0c8
jyg41 <j.y.g.411@gmail.com>
2020-01-09 10:12:12 +0900

    room as asynchronous

--- app/src/main/java/com/box/room_ex/MainActivity.kt
+++ app/src/main/java/com/box/room_ex/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.box.room_ex
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.RoomMasterTable
+import com.box.room_exe.TodoDao
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,19 +22,23 @@ class MainActivity : AppCompatActivity() {
             this@MainActivity,
             AppDatabase::class.java,
             "todo-db"
-        ).allowMainThreadQueries().build()
+        ).build()
 
         //refresh UI
         db.todoDao().getAll().observe(this,
             Observer {
                 result_text.text = it.toString()
             })
+
         add_btn.setOnClickListener {
-            db.todoDao().insert(Todo(todo_edit.text.toString()))
+            lifecycleScope.launch(Dispatchers.IO) {
+                db.todoDao().insert(Todo(todo_edit.text.toString()))
+            }
         }
         remove_btn.setOnClickListener {
-            db.todoDao().delete(todo_edit.text.toString().toInt())
+            lifecycleScope.launch(Dispatchers.IO) {
+                 db.todoDao().delete(todo_edit.text.toString().toInt())
+            }
         }
     }
-
 }
